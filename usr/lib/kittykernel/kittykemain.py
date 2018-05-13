73a9acc47c309b405d502396fa55f72af4e1a164
Schallaven <sven.kochmann@gmx.de>
2018-05-12 23:19:44 -0400

    Change version to 1.3

--- usr/lib/kittykernel/kittykemain.py
+++ usr/lib/kittykernel/kittykemain.py
@@ -474,7 +474,7 @@ class KittykeMainWindow():
         dlg.set_logo(GdkPixbuf.Pixbuf.new_from_file_at_scale("/usr/lib/kittykernel/kittykernel.svg", 256, 256, True))
         dlg.set_website("http://www.github.com/schallaven/kittykernel")
         dlg.set_transient_for(self.window)        
-        dlg.set_version("1.2")
+        dlg.set_version("1.3")
         dlg.set_license_type(Gtk.License.GPL_3_0)
 
         # Contributors, who contributed in form of PRs
